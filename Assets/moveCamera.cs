7b2a2032536253efab76e5b207178f67100afdc3
GustavoGuterres <gusags@hotmail.com>
2018-01-27 05:06:50 -0200

    Add 1
    Scene teste, game manager, camera free - follow

---
+++ Assets/moveCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCamera : MonoBehaviour {
+
+    public bool canMove;
+    public float speed;
+    private float moveHorizontal;
+    private Rigidbody rb;
+    public GameObject ball;
+
+    void Start() {
+        canMove = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (canMove) {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+            rb.velocity = (movement * speed);
+        } else {
+            transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, transform.position.z);
+        }
+    }
+
+}
