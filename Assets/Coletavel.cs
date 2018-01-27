391d321a24fb7fa902572f677543c9db90c293e4
GustavoGuterres <gusags@hotmail.com>
2018-01-27 13:14:43 -0200

    add 2

---
+++ Assets/Coletavel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletavel : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "Coletavel") {
+            Debug.Log("pontos");
+        }
+    }
+}
--- Assets/moveCamera.cs
+++ Assets/moveCamera.cs
@@ -9,6 +9,8 @@ public class moveCamera : MonoBehaviour {
     private float moveHorizontal;
     private Rigidbody rb;
     public GameObject ball;
+    public Transform farLeftCamera;
+    public Transform farRightCamera;
 
     void Start() {
         canMove = true;
@@ -20,6 +22,13 @@ public class moveCamera : MonoBehaviour {
             moveHorizontal = Input.GetAxis("Horizontal");
             Vector3 movement = new Vector3(moveHorizontal, 0, 0);
             rb.velocity = (movement * speed);
+            if (transform.position.x > farRightCamera.position.x) {
+                transform.position = new Vector3(farRightCamera.position.x, transform.position.y, transform.position.z);
+            } else {
+                if (transform.position.x < farLeftCamera.position.x) {
+                    transform.position = new Vector3(farLeftCamera.position.x, transform.position.y, transform.position.z);
+                }
+            }
         } else {
             transform.position = new Vector3(ball.transform.position.x, ball.transform.position.y, transform.position.z);
         }
