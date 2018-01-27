2f366386ad7cdf9aa4cf9c84ffcc310b7b99ab13
Gabriel Maia <gmaia71@gmail.com>
2018-01-27 16:07:21 -0200

    Viramos 2D boyz

--- Assets/ScriptBola.cs
+++ Assets/ScriptBola.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class ScriptBola : MonoBehaviour {
 
-	
+	Rigidbody2D rigid;
+
+	bool reversed;
+
+	public float velocidade, velocidadeMax;
 	// Use this for initialization
 	void Start () {
-		
+		rigid = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (rigid.velocity.x < velocidadeMax || rigid.velocity.x < -velocidadeMax)
+		{
+			if (!reversed)
+			{
+				rigid.AddTorque(-velocidade, ForceMode2D.Force);
+			}
+			else
+			{
+				rigid.AddTorque(velocidade, ForceMode2D.Force);
+			}
+		}
 	}
 }
