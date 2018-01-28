a7c8b364d69e168f379ca9e10d635277259a8b17
GustavoGuterres <gusags@hotmail.com>
2018-01-28 16:21:02 -0200

    menu

---
+++ Assets/MenuStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour {
+    
+    public Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    } 
+
+    public void ProxFase() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartGame() {
+        anim.SetTrigger("Go");
+    }
+}
